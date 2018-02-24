5c39b98ed248f4f33fa2eda106766f421800ad72
pierceyouatt <pierce.youatt@chicagobooth.edu>
2018-02-23 21:10:20 -0500

    Mid-class commit

---
+++ app/models/bookmark.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: bookmarks
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  movie_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belogns_to :user
+end
